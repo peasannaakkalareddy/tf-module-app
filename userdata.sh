556ea7cd513008f9c10e0fe9c9725c0cdf55c25b
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-12 18:09:16 +0530

    roboshop - ansible

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/
 
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/prasannaakkal areddy/roboshop-ansible roboshop.yml -e role_name=${name} -e env=${env} &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/peasannaakkalareddy/roboshop-ansible roboshop.yml -e role_name=${name} -e env=${env} &>>/opt/ansible.log
