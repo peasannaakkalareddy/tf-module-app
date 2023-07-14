b8192601d86b8f3cbb3e5aabed0de67e7e85ec0a
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-14 18:38:13 +0530

    roboshop - ansible

--- vars.tf
+++ vars.tf
@@ -5,6 +5,7 @@ variable "env" {}
 variable "vpc_id" {}
 variable "allow_app_cidr" {}
 variable "bastion_cidr" {}
+variable "monitor_cidr" {}
 variable "desired_capacity" {}
 variable "max_size" {}
 variable "min_size" {}
